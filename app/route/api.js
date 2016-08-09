16c14c6a1a64c01e44d1dffffe8ac9e921f1fcb8
bface007 <bface007@gmail.com>
2016-08-09 15:16:47 +0100

    add messages endpoint to api route

--- app/route/api.js
+++ app/route/api.js
@@ -1,6 +1,8 @@
 /**
  * Created by bface007 on 24/07/2016.
  */
+var Message = require( '../model/message' );
+
 module.exports = function (app, express) {
     var router = express.Router();
 
@@ -9,6 +11,18 @@ module.exports = function (app, express) {
         console.log("Route from ", req.routes_from);
         next();
     });
+
+    router
+        .get( '/messages', function ( req, res ) {
+            Message.find( {} ).populate( "__author" ).sort( "-created" ).exec( function ( err, messages ) {
+                if( err ) {
+                    console.error("api get messages ", err);
+                    return res.send(err);
+                }
+
+                res.json( messages );
+            } )
+        } );
     
     return router;
 };
